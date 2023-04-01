694d9356f9c542641691da594db411073f018b3b
quhaaST <asndjabbd@gmail.com>
2023-04-01 20:37:19 +0300

    Updated tests

--- src/test/kotlin/org/stella/MainTest.kt
+++ src/test/kotlin/org/stella/MainTest.kt
@@ -7,25 +7,30 @@ import java.io.FileInputStream
 import java.io.IOException
 
 const val baseDir = "src/test/resources"
-const val wellTypedDir = "$baseDir/well-typed"
-const val illTypedDir = "$baseDir/ill-typed"
+const val coreBaseDir = "$baseDir/core"
+const val listsBaseDir = "$baseDir/lists"
+const val pairsBaseDir = "$baseDir/pairs"
+const val recordsBaseDir = "$baseDir/records"
+const val sumTypesBaseDir = "$baseDir/sum-types"
+const val tuplesBaseDir = "$baseDir/tuples"
+const val variantsBaseDir = "$baseDir/variants"
 
 internal class MainTest {
-    @ParameterizedTest(name = "{index} Typechecking well-typed program {0}")
+    @ParameterizedTest(name = "{index} Typechecking well-typed core program {0}")
     @ValueSource(strings = [
-        "$wellTypedDir/factorial.stella",
-        "$wellTypedDir/squares.stella",
-        "$wellTypedDir/higher-order-1.stella",
-        "$wellTypedDir/increment_twice.stella",
-        "$wellTypedDir/logical-operators.stella",
-        "$wellTypedDir/biconditional.stella",
-        "$wellTypedDir/power-of-two.stella",
+        "$coreBaseDir/well-typed/factorial.stella",
+        "$coreBaseDir/well-typed/squares.stella",
+        "$coreBaseDir/well-typed/higher-order-1.stella",
+        "$coreBaseDir/well-typed/increment_twice.stella",
+        "$coreBaseDir/well-typed/logical-operators.stella",
+        "$coreBaseDir/well-typed/biconditional.stella",
+        "$coreBaseDir/well-typed/power-of-two.stella",
     ])
     @Throws(
         IOException::class,
         Exception::class
     )
-    fun testWellTyped(filepath: String) {
+    fun testWellTypedCore(filepath: String) {
         val original = System.`in`
         val fips = FileInputStream(File(filepath))
         System.setIn(fips)
@@ -33,32 +38,72 @@ internal class MainTest {
         System.setIn(original)
     }
 
-    @ParameterizedTest(name = "{index} Typechecking ill-typed program {0}")
+    @ParameterizedTest(name = "{index} Typechecking ill-typed core program {0}")
     @ValueSource(strings = [
-        "$illTypedDir/applying-non-function-1.stella",
-        "$illTypedDir/applying-non-function-2.stella",
-        "$illTypedDir/applying-non-function-3.stella",
-        "$illTypedDir/argument-type-mismatch-1.stella",
-        "$illTypedDir/argument-type-mismatch-2.stella",
-        "$illTypedDir/argument-type-mismatch-3.stella",
-        "$illTypedDir/bad-if-1.stella",
-        "$illTypedDir/bad-if-2.stella",
-        "$illTypedDir/bad-succ-1.stella",
-        "$illTypedDir/bad-succ-2.stella",
-        "$illTypedDir/bad-succ-3.stella",
-        "$illTypedDir/shadowed-variable-1.stella",
-        "$illTypedDir/undefined-variable-1.stella",
-        "$illTypedDir/undefined-variable-2.stella",
-        "$illTypedDir/bad-squares-1.stella",
-        "$illTypedDir/bad-squares-2.stella",
-        "$illTypedDir/missing-main-method.stella",
-        "$illTypedDir/undefined-function.stella",
+        "$coreBaseDir/ill-typed/applying-non-function-1.stella",
+        "$coreBaseDir/ill-typed/applying-non-function-2.stella",
+        "$coreBaseDir/ill-typed/applying-non-function-3.stella",
+        "$coreBaseDir/ill-typed/argument-type-mismatch-1.stella",
+        "$coreBaseDir/ill-typed/argument-type-mismatch-2.stella",
+        "$coreBaseDir/ill-typed/argument-type-mismatch-3.stella",
+        "$coreBaseDir/ill-typed/bad-if-1.stella",
+        "$coreBaseDir/ill-typed/bad-if-2.stella",
+        "$coreBaseDir/ill-typed/bad-succ-1.stella",
+        "$coreBaseDir/ill-typed/bad-succ-2.stella",
+        "$coreBaseDir/ill-typed/bad-succ-3.stella",
+        "$coreBaseDir/ill-typed/shadowed-variable-1.stella",
+        "$coreBaseDir/ill-typed/undefined-variable-1.stella",
+        "$coreBaseDir/ill-typed/undefined-variable-2.stella",
+        "$coreBaseDir/ill-typed/bad-squares-1.stella",
+        "$coreBaseDir/ill-typed/bad-squares-2.stella",
+        "$coreBaseDir/ill-typed/missing-main-method.stella",
+        "$coreBaseDir/ill-typed/undefined-function.stella",
     ])
     @Throws(
         IOException::class,
         Exception::class
     )
-    fun testIllTyped(filepath: String) {
+    fun testIllTypedCore(filepath: String) {
+        val original = System.`in`
+        val fips = FileInputStream(File(filepath))
+        System.setIn(fips)
+        var typecheckerFailed = false
+        try {
+            main()
+        } catch (e: java.lang.Exception) {
+            typecheckerFailed = true
+        }
+        if (!typecheckerFailed) {
+            throw java.lang.Exception("expected the typechecker to fail!")
+        }        // TODO: check that there is a type error actually, and not a problem with implementation
+        System.setIn(original)
+    }
+
+    @ParameterizedTest(name = "{index} Typechecking well-typed lists program {0}")
+    @ValueSource(strings = [
+        "$listsBaseDir/well-typed/lists-1.stella",
+    ])
+    @Throws(
+        IOException::class,
+        Exception::class
+    )
+    fun testWellTypedLists(filepath: String) {
+        val original = System.`in`
+        val fips = FileInputStream(File(filepath))
+        System.setIn(fips)
+        main()
+        System.setIn(original)
+    }
+
+    @ParameterizedTest(name = "{index} Typechecking ill-typed lists program {0}")
+    @ValueSource(strings = [
+        "$listsBaseDir/ill-typed/bad-variants-1.stella",
+    ])
+    @Throws(
+        IOException::class,
+        Exception::class
+    )
+    fun testIllTypedLists(filepath: String) {
         val original = System.`in`
         val fips = FileInputStream(File(filepath))
         System.setIn(fips)
